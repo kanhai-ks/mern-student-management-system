edb350aa19dbcbd9bcfe2a598b411e0da2e0111b
kanhai-ks <kanhai.k.sah@gmail.com>
2026-02-04 17:44:48 +0545

    setup mern stack crud operation project

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-center text-amber-300 text-5xl">
+        hello, crud operation{" "}
+      </h1>
+    </div>
+  );
+};
+
+export default App;
