073aa6cddda455556d3f81f88cfafe1bd8222d0e
kanhai-ks <kanhai.k.sah@gmail.com>
2026-03-26 18:46:23 +0545

    update

--- server/middleware/multer.js
+++ server/middleware/multer.js
@@ -2,13 +2,10 @@
 
 import multer from "multer";
 import path from "path";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, process.env.UPLOADS_FOLDER);
+    cb(null, uploads);
   },
   // Filename format: current timestamp + original file extension
   filename: (req, file, cb) => {
